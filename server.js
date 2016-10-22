8bd965314c68eeadda3787eda0c964756ee40c30
oknevor <rowenko1000@gmail.com>
2016-10-22 17:25:04 +0300

    Create and Read

--- server.js
+++ server.js
@@ -4,50 +4,99 @@ const bodyParser = require('body-parser');
 const app = express();
 var quotes = [];
 
+var fs = require('fs');
+//-----------------------blocks of code----------------------
+var html = ``,
+	style = ``,
+	htmlToInsert = ``;
 
-
-var htmlStringStart,
-	htmlToInsert,
-	htmlStringEnd;
-
-	htmlStringStart = "
-
+function initialize(){
+	html = `
 <!DOCTYPE html>
-<html lang='en'>
+<html lang="en">
 <head>
-	<meta charset='UTF-8'>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style>
+		.quote {
+  border: 1px solid #000000;
+  margin: 5px;
+  padding: 5px;
+  width: 200px;
+  height: 100px;
+  float: left;
+  overflow: scroll;
+}
+${style}
+	</style>
 	<title>FirstDocument</title>
 </head>
 <body>
 	Hello again!
-	<form action='/quotes' method='POST'>
-		<input type='text' name='name' placeholder='name'>
-		<input type='text' name='quote' placeholder='quote'>
-		<button type='submit'>Submit</button>
+	<form action="/quotes" method="POST">
+		<input type="text" name="name" placeholder="name">
+		<input type="text" name="quote" placeholder="quote">
+		<button type="submit">Submit</button>
 	</form>
-";
+	${htmlToInsert}
+</body>
+</html>
 
-	htmlToInsert = ' ';
+	`//-------------------------endHTML
+}
 
-	htmlStringEnd = '
+/*
+	htmlStringStart = `
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style>
+		.quote {
+  border: 1px solid #000000;
+  margin: 5px;
+  padding: 5px;
+  width: 200px;
+  height: 100px;
+  float: left;
+  overflow: scroll;
+}
+${style}
+	</style>
+	<title>FirstDocument</title>
+</head>
+<body>
+	Hello again!
+	<form action="/quotes" method="POST">
+		<input type="text" name="name" placeholder="name">
+		<input type="text" name="quote" placeholder="quote">
+		<button type="submit">Submit</button>
+	</form>
+`;
+
+	htmlToInsert = ` `;
+
+	htmlStringEnd = `
 
 </body>
 </html>
 
-';
-
-
-
-
+`;
+*/
+//-----------------------writing files in FS----------------------
+/*
 var fs = require('fs');
-fs.writeFile(__dirname + "/tmp/test", "Hey there!", function(err) {
+fs.writeFile(__dirname + "/tmp/test", "go away", function(err) {
     if(err) {
         return console.log(err);
     }
 
     console.log("The file was saved!");
 }); 
-
+*/
+//----------------------express
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function() {
@@ -66,10 +115,11 @@ app.post('/quotes', (req, res) => {
 	console.log(tmp.name, tmp.quote, tmp.color);
 	console.log(quotes);
 	//console.log(req.body);
+	//style = addStyle();//------------------Not here
 	refreshHTML();
 	res.redirect('/');
 })
-
+//---------------------colorize---------------------------
 function randomDigit(){
 	var floatRandom = 155+ Math.random()*100;
 	//it should be quite bright, thatswhy 155+
@@ -84,25 +134,47 @@ function randomColor(){
 	var blue = randomDigit();
 	return ("#"+red+green+blue)
 }
-
+//---------------------adding changes to HTML--------------
 function refreshHTML(){
+	htmlToInsert = ``;
+	style = ``;
 	for (var i = 0; i < quotes.length; i++){
-		addItemtoHTML(i);
+		addItemToHTML(i);
 	}
+	initialize();
+	//var finalHTML = htmlStringStart+htmlToInsert+htmlStringEnd;
+
+	fs.writeFile(__dirname + "/index.html", html, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
 }
 
 function addItemToHTML(i){
 	htmlToInsert = htmlToInsert.concat(wrap(i));
+	style = style.concat(addStyle(i));
 }
 
 function wrap(i){
-	var str = '
-		<div class="quote" background-color="'+quotes[i].color+'">
-			'quotes[i].name'<br>
-			'quotes[i].quote'
+	var str = `
+		<div class="quote" id="_${i}">
+			${quotes[i].name}<br>
+			${quotes[i].quote}
 		</div>
-	';
+	`;
 
-	return 
+	return str;
+}
+
+function addStyle(i){
+	var str = `
+		#_${i}{
+			background-color: ${quotes[i].color};
+		}
+	`
+	return str;
 }
 
